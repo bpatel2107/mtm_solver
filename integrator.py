b5f9f6489ed30942f87d0f143756849560549fce
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2021-08-20 11:57:37 +0100

    Add minimiser and scan through ky

--- integrator.py
+++ integrator.py
@@ -73,7 +73,7 @@ def imag_integrand(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel
     return np.imag(inte)
 
 
-def mtm_integral(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu):
+def mtm_integral(*args):
 
     real_integral = dblquad(real_integrand, 0, np.inf, lower_bound_xpar, upper_bound_xpar, args=args)
 
@@ -96,9 +96,6 @@ def minimise_func(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, kperp):
 
 if __name__=='__main__':
 
-    # STEP parameters
-    n = 5
-
     r = 1.027
     R = 2.775
     q = 4.3
@@ -108,21 +105,10 @@ if __name__=='__main__':
     gte = 4.9578
     gne = 0.7620
 
-    ky = n * q / r
-
-    kperp = ky
-
     ne = 15.2e19
     te = 12.173e3 * e_charge
-    rho = 0.66
+    rho = 0.6627
     minor_radius = r / rho
-
-    sound_speed = np.sqrt(te / deuterium_mass )
-    ion_freq = e_charge * btor / deuterium_mass
-    rho_star = sound_speed / ion_freq
-
-    eta = gte / gne
-    omega_star = (ky * te * gne / (e_charge * btor * R))
 
     k_parallel = 0.0
 
@@ -131,22 +117,69 @@ if __name__=='__main__':
     coolog = 24 - np.log(np.sqrt(ne * 1e-6) / te * e_charge )
     nu = 4 * (2 * np.pi)**0.5 * ne * coolog * e_charge**4 * zeff / (3 * (4 * np.pi * epsilon0)**2 * electron_mass**0.5 *
                                                                     te**1.5)
-    alpha0 = 4.361
+    # CGYRO units
+    sound_speed = np.sqrt(te / deuterium_mass )
+    ion_freq = e_charge * bunit / deuterium_mass
+    larmor_radius = sound_speed / ion_freq
+    rho_star = larmor_radius / minor_radius
 
-    # Initial guess for omega
-    omega = (nu**2 * omega_star * (1 + eta) + 1j * alpha0 * eta * omega_star * nu * (1 + eta) ) / (nu**2 + alpha0 * eta * omega_star)
+    eta = gte / gne
 
-    omega = omega_star * (1 + eta) * (1 + 1j /22)
-    print(
-        f"\nInitial guess: omega = {np.real(omega) / sound_speed * minor_radius:.2f}, gamma = {np.imag(omega) / sound_speed * minor_radius:.2f}")
+    nky = 15
+    delta_n = 10
+    kyrhos = np.empty(nky)
+    omegas = np.empty(nky)
+    gammas = np.empty(nky)
 
-    # Perform integral
-    integral, error = mtm_integral(omega, ky, btor, R, r, gte, gne, te, ne, k_parallel, nu)
-    print(integral)
+    integration_diff = np.empty(nky)
 
-    # Test minimiser
-    minimiser = minimise_func(omega, ky, btor, R, r, gte, gne, te, ne, k_parallel, nu, kperp)
-    print(minimiser)
+    for n in range(nky):
 
-    root = newton(minimise_func, omega, args=(ky, btor, R, r, gte, gne, te, ne, k_parallel, nu, kperp))
-    print(root)
+        ky = delta_n * (n + 1) * q / r
+        kperp = ky
+        omega_star = (ky * te * gne / (e_charge * btor * R))
+
+        # Initial guess for omega_guess
+        omega_guess = omega_star * (1 + eta)
+
+        kyrhos[n] = ky * larmor_radius
+        print(f'kyrhos = {kyrhos[n]}')
+        print(
+            f"Initial guess: omega_guess = {np.real(omega_guess) / sound_speed * minor_radius:.2f}, gamma = {np.imag(omega_guess) / sound_speed * minor_radius:.2f}")
+
+        # Run minimiser
+        omega = newton(minimise_func, omega_guess, args=(ky, btor, R, r, gte, gne, te, ne, k_parallel, nu, kperp))
+        print(f"{omega / sound_speed * minor_radius}\n")
+
+        omegas[n] = np.real(omega / sound_speed * minor_radius)
+        gammas[n] = np.imag(omega / sound_speed * minor_radius)
+
+        # Test result
+        minimiser = minimise_func(omega, ky, btor, R, r, gte, gne, te, ne, k_parallel, nu, kperp)
+        integration_diff[n] = abs(minimiser)
+
+    # Save eigenvalues to file
+    output = np.vstack((kyrhos, gammas,  omegas))
+    np.savetxt('mtm-integrator_eigval.dat', np.transpose(output), header='MTM-integrator ky')
+
+    fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(8,7))
+
+    ax1.plot(kyrhos, gammas)
+    ax1.set_ylabel(r'$c_s/a$')
+    ax1.set_xlabel(r'$k_y\rho_s$')
+    ax1.set_title(r'$\gamma$')
+
+    ax2.plot(kyrhos, omegas)
+    ax2.set_ylabel(r'$c_s/a$')
+    ax2.set_xlabel(r'$k_y\rho_s$')
+    ax2.set_title(r'$\omega$')
+
+    plt.tight_layout()
+    plt.show()
+
+    plt.plot(kyrhos, integration_diff)
+    plt.xlabel(r'$k_y\rho_s$')
+    plt.title('Difference in integration')
+    plt.yscale('log')
+    plt.show()
+
