308519e167444aeba6e3008787a4f3335a1b98bf
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2021-08-27 09:46:15 +0100

    Added bessel function, omega_drift term, changed field to Bunit, fixed collisionality defn

--- integrator.py
+++ integrator.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import dblquad
 from scipy.optimize import newton, minimize
+from scipy.special import j0
 import matplotlib.pyplot as plt
 
 
@@ -29,7 +30,9 @@ def integrand(xpar,
               te,
               ne,
               k_parallel,
-              nu):
+              nu,
+              bessel,
+              q):
 
     v_thermal = np.sqrt(2 * te / electron_mass)
 
@@ -38,20 +41,22 @@ def integrand(xpar,
     # x = v / vth
     x = np.sqrt(xpar ** 2 + xperp ** 2)
 
-    eta = gte / gne
-
     # Maxwellian
-    f0 = np.exp(-x ** 2)
+    f0 = np.exp(-x ** 2) * bessel **2
 
-    omega_star = ky * te * gne / (e_charge * B * R)
-    omega_star_x = omega_star * (1 + eta * (x ** 2 - 1.5))
+    omega_star = - ky * te / (e_charge * B * R)
+    omega_star_x = omega_star * (gne + gte * (x ** 2 - 1.5))
 
     omega_drift = 2 * ky * v_thermal / R ** 2 * (xpar ** 2 + 0.5 * xperp ** 2)
     omega_drift_x = omega_drift * 0.0
+    omega_drift_x = 2 * ky * larmor_radius * sound_speed * r / R**2 * (1 - 1/q**2)
 
     nu_v = 1j * nu / epsilon / x ** 3
 
     pre_factor = - 2 * ne * e_charge**2 * v_thermal**2 / (np.sqrt(np.pi) * te)
+
+    omega_drift_x = 0.0
+    k_parallel = 0.0
 
     integrand = pre_factor * xpar ** 2 * xperp * f0 * (omega - omega_star_x) / (
             omega - k_parallel * xpar - omega_drift_x + nu_v)
@@ -60,9 +65,9 @@ def integrand(xpar,
     return integrand
 
 
-def integrand_component(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, func):
+def integrand_component(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q, func):
 
-    return func(integrand(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu))
+    return func(integrand(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q))
 
 
 def mtm_integral(*args):
@@ -79,78 +84,103 @@ def mtm_integral(*args):
     return complex_integral, complex_error
 
 
-def minimise_func(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, kperp):
+def minimise_func(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q, kperp):
 
-    integral, error =  mtm_integral(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu)
+    integral, error =  mtm_integral(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q)
 
-    minimse_value = integral - kperp ** 2 / mu0
+    minimise_value = integral - kperp ** 2 / mu0
 
-    return minimse_value
+    return minimise_value
 
 
 if __name__=='__main__':
 
+    # STEP Parameters
     r = 1.027
     R = 2.775
     q = 4.3
-
-    btor = 2.162
+    shear = 0.74
     bunit = 7.52
+    btor = 2.162
+
     gte = 4.9578
     gne = 0.7620
 
-    ne = 15.2e19
+    ne = 15.1e19
     te = 12.173e3 * e_charge
     rho = 0.6627
+
+    # NSTX Parameters
+    r = 0.37
+    R = 0.94
+    q = 1.7
+    shear = 1.7
+    bunit = 0.666
+    btor = 0.35
+
+    gte = 4.1
+    gne = 0.0
+
+    ne = 6e19
+    te = 0.45 * 1e3 * e_charge
+    rho = 0.6
+
     minor_radius = r / rho
 
-    k_parallel = 0.0
-
-    # Collisionality
-    zeff = 1.0
-    coolog = 24 - np.log(np.sqrt(ne * 1e-6) / te * e_charge )
-    nu = 4 * (2 * np.pi)**0.5 * ne * coolog * e_charge**4 * zeff / (3 * (4 * np.pi * epsilon0)**2 * electron_mass**0.5 *
-                                                                    te**1.5)
     # CGYRO units
     sound_speed = np.sqrt(te / deuterium_mass )
     ion_freq = e_charge * bunit / deuterium_mass
     larmor_radius = sound_speed / ion_freq
     rho_star = larmor_radius / minor_radius
 
-    eta = gte / gne
+    # Collisionality
+    zeff = 1.0
+    coolog = 24 - np.log(np.sqrt(ne* 1e-6) / (te / e_charge ) )
 
-    nky = 15
-    delta_n = 10
+    nu = np.sqrt(2) * np.pi * ne * coolog * e_charge ** 4 / (
+                 (4 * np.pi * epsilon0) ** 2 * electron_mass ** 0.5 *
+                 te ** 1.5)
+
+    nky = 2
+    delta_n = 5
     kyrhos = np.empty(nky)
     omegas = np.empty(nky)
     gammas = np.empty(nky)
 
     integration_diff = np.empty(nky)
 
-    for n in range(nky):
+    for i in range(nky):
 
-        ky = delta_n * (n + 1) * q / r
-        kperp = ky
-        omega_star = (ky * te * gne / (e_charge * btor * R))
+        n = (i + 1) * delta_n
+        ky = n * q / r
+
+        kx_over_ky = 1.0/0.2
+        kperp = ky * np.sqrt(1**2 + kx_over_ky**2)
+        omega_star = - (ky * te / (e_charge * bunit * R))
+
+        k_parallel = 2 * shear / (R * q * kx_over_ky**2)
 
         # Initial guess for omega_guess
-        omega_guess = omega_star * (1 + eta)
+        omega_guess = omega_star * (gne + gte/2)
+        #omega_guess = 0.0
+        # Set up bessel function
+        kyrhos[i] = ky * larmor_radius
+        bessel = j0(kyrhos[i])
 
-        kyrhos[n] = ky * larmor_radius
-        print(f'kyrhos = {kyrhos[n]}')
+        print(f'kyrhos = {kyrhos[i]}')
         print(
             f"Initial guess: omega_guess = {np.real(omega_guess) / sound_speed * minor_radius:.2f}, gamma = {np.imag(omega_guess) / sound_speed * minor_radius:.2f}")
 
         # Run minimiser
-        omega = newton(minimise_func, omega_guess, args=(ky, btor, R, r, gte, gne, te, ne, k_parallel, nu, kperp))
+        omega = newton(minimise_func, omega_guess, args=(ky, bunit, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q, kperp))
         print(f"{omega / sound_speed * minor_radius}\n")
 
-        omegas[n] = np.real(omega / sound_speed * minor_radius)
-        gammas[n] = np.imag(omega / sound_speed * minor_radius)
+        omegas[i] = np.real(omega / sound_speed * minor_radius)
+        gammas[i] = np.imag(omega / sound_speed * minor_radius)
 
         # Test result
-        minimiser = minimise_func(omega, ky, btor, R, r, gte, gne, te, ne, k_parallel, nu, kperp)
-        integration_diff[n] = abs(minimiser)
+        minimiser = minimise_func(omega, ky, bunit, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q, kperp)
+        integration_diff[i] = abs(minimiser)
 
     # Save eigenvalues to file
     output = np.vstack((kyrhos, gammas,  omegas))
