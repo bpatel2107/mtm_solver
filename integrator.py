f63f6e849bb677451ad3940bbafbf62cd46b0a57
Bhavin Patel <15210802+bpatel2107@users.noreply.github.com>
2022-02-16 15:44:53 +0000

    Add phi into integration, remove redundant integrals

--- integrator.py
+++ integrator.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-
-
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy.integrate import dblquad
@@ -17,7 +15,6 @@ electron_mass = 9.1e-31
 epsilon0 = 8.854e-12
 deuterium_mass = 2 * 1.67e-27
 
-phi_switch = 0
 
 def lower_bound_xpar(xperp):
     return -np.inf
@@ -25,10 +22,7 @@ def lower_bound_xpar(xperp):
 def upper_bound_xpar(xperp):
     return np.inf
 
-
-
-
-def integrand_1(xpar,
+def integrand(xpar,
               xperp,
               omega,
               ky,
@@ -65,156 +59,29 @@ def integrand_1(xpar,
     
     nu_v = 1j * nu / epsilon / (x**3)  
 
-    pre_factor_1 = 2 * ne * e_charge**2 * v_thermal**2 / (np.sqrt(np.pi) * te)
+    v_alfven = B / np.sqrt(ne * mu0 * deuterium_mass)
 
-    integrand_1 = pre_factor_1 * xpar ** 2 * xperp * f0 * (omega - omega_star_x) / (
+    pre_factor = 2 * ne * e_charge**2 * v_thermal**2 / (np.sqrt(np.pi) * te)
+
+    integrand = pre_factor * (xpar ** 2 - xpar * k_parallel * v_alfven**2 / (omega * v_thermal)) * xperp * f0 * (omega - omega_star_x) / (
             omega - k_parallel * xpar - omega_drift_x + nu_v)
 
     # Return only real part for integration
-    return integrand_1
+    return integrand
 
 
 
+def integrand_component(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q, func):
 
-def integrand_2(xpar,
-              xperp,
-              omega,
-              ky,
-              B,
-              R,
-              r,
-              gte,
-              gne,
-              te,
-              ne,
-              k_parallel,
-              nu,
-              bessel,
-              q):
-
-    v_thermal = np.sqrt(2 * te / electron_mass)
-
-    epsilon = r / R
-
-    # x = v / vth
-    x = np.sqrt(xpar ** 2 + xperp ** 2)
-
-    # Maxwellian
-    f0 = np.exp(-1 * x ** 2) * (j0(-ky*xperp*v_thermal * electron_mass / (e_charge*B))) ** 2
-
-    omega_star = -1 * ky * te / (e_charge * B * R)
-    omega_star_x = omega_star * (gne + gte * (x ** 2 - 1.5))
-
-    omega_drift = 2 * ky * v_thermal / R ** 2 * (xpar ** 2 + 0.5 * xperp ** 2)
-    omega_drift_x = omega_drift * 0.0
-    omega_drift_x = 2 * ky * larmor_radius * sound_speed * r / R**2 * (1 - 1/q**2)
+    return func(integrand(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q))
 
 
-    nu_v = 1j * nu / epsilon / (x**3)  
-
-    pre_factor_2 = 2 / np.sqrt(np.pi)
-
-    integrand_2 = pre_factor_2 * xperp * f0 * (omega - omega_star_x) / (
-            omega - k_parallel * xpar - omega_drift_x + nu_v)
-
-    # Return only real part for integration
-    return integrand_2
-
-
-
-
-def integrand_3(xpar,
-              xperp,
-              omega,
-              ky,
-              B,
-              R,
-              r,
-              gte,
-              gne,
-              te,
-              ne,
-              k_parallel,
-              nu,
-              bessel,
-              q):
-
-    v_thermal = np.sqrt(2 * te / electron_mass)
-
-    epsilon = r / R
-
-    # x = v / vth
-    x = np.sqrt(xpar ** 2 + xperp ** 2)
-
-    # Maxwellian
-    f0 = np.exp(-1 * x ** 2) * (j0(-ky*xperp*v_thermal * electron_mass / (e_charge*B))) ** 2
-
-    omega_star = -1 * ky * te / (e_charge * B * R)
-    omega_star_x = omega_star * (gne + gte * (x ** 2 - 1.5))
-
-    omega_drift = 2 * ky * v_thermal / R ** 2 * (xpar ** 2 + 0.5 * xperp ** 2)
-    omega_drift_x = omega_drift * 0.0
-    omega_drift_x = 2 * ky * larmor_radius * sound_speed * r / R**2 * (1 - 1/q**2)
-
-    nu_v = 1j * nu / epsilon / (x**3)  
-
-    pre_factor_3 = 2 * v_thermal / np.sqrt(np.pi)
-
-    integrand_3 = pre_factor_3 * xpar * xperp * f0 * (omega - omega_star_x) / (
-            omega - k_parallel * xpar - omega_drift_x + nu_v)
-
-    # Return only real part for integration
-    return integrand_3
-
-
-
-
-def integrand_component_1(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q, func):
-
-    return func(integrand_1(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q))
-
-def integrand_component_2(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q, func):
-
-    return func(integrand_2(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q))
-
-def integrand_component_3(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q, func):
-
-    return func(integrand_3(xpar, xperp, omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q))
-
-
-# In[7]:
-
-
-def mtm_integral_1(*args):
+def mtm_integral(*args):
 
     real_args = args + (np.real,)
-    real_integral = dblquad(integrand_component_1, 0, np.inf, lower_bound_xpar, upper_bound_xpar, args=real_args)  
+    real_integral = dblquad(integrand_component, 0, np.inf, lower_bound_xpar, upper_bound_xpar, args=real_args)
     imag_args = args + (np.imag,)
-    imag_integral = dblquad(integrand_component_1, 0, np.inf, lower_bound_xpar, upper_bound_xpar, args=imag_args)
-    
-    complex_integral = real_integral[0] + 1j * imag_integral[0]
-    complex_error = real_integral[1] + 1j * imag_integral[1]
-
-    return complex_integral, complex_error
-
-def mtm_integral_2(*args):
-
-    real_args = args + (np.real,)
-    real_integral = dblquad(integrand_component_2, 0, np.inf, lower_bound_xpar, upper_bound_xpar, args=real_args)  
-    imag_args = args + (np.imag,)
-    imag_integral = dblquad(integrand_component_2, 0, np.inf, lower_bound_xpar, upper_bound_xpar, args=imag_args)
-    
-    complex_integral = real_integral[0] + 1j * imag_integral[0]
-    complex_error = real_integral[1] + 1j * imag_integral[1]
-
-    return complex_integral, complex_error
-
-def mtm_integral_3(*args):
-
-    real_args = args + (np.real,)
-    real_integral = dblquad(integrand_component_3, 0, np.inf, lower_bound_xpar, upper_bound_xpar, args=real_args)  
-    imag_args = args + (np.imag,)
-    imag_integral = dblquad(integrand_component_3, 0, np.inf, lower_bound_xpar, upper_bound_xpar, args=imag_args)
+    imag_integral = dblquad(integrand_component, 0, np.inf, lower_bound_xpar, upper_bound_xpar, args=imag_args)
     
     complex_integral = real_integral[0] + 1j * imag_integral[0]
     complex_error = real_integral[1] + 1j * imag_integral[1]
@@ -224,27 +91,18 @@ def mtm_integral_3(*args):
 
 def minimise_func(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q, kperp):
 
-    integral_1, error_1 =  mtm_integral_1(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q)
-    integral_2, error_2 =  mtm_integral_2(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q)
-    integral_3, error_3 =  mtm_integral_3(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q)
+    integral, error =  mtm_integral(omega, ky, B, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q)
 
-    if phi_switch != 0:
-        # need to check signs
-        minimse_value = ((ne * (e_charge ** 2) / te) * integral_3 ** 2 ) - (1 + (epsilon0 * te / (ne * e_charge ** 2)) * kperp ** 2 - integral_2) * (integral_1 + kperp ** 2 / mu0)    
-    elif phi_switch == 0: 
-        minimse_value = (integral_1 + kperp ** 2 / mu0)
-
-    
+    minimse_value = (integral + kperp ** 2 / mu0)
 
     return minimse_value
 
 
-
-
 if __name__=='__main__':
 
-    
- # STEP Parameters
+    """
+    # STEP Parameters
+    device = 'STEP'
     r = 1.027
     R = 2.775
     q = 4.3
@@ -258,8 +116,9 @@ if __name__=='__main__':
     ne = 15.1e19
     te = 12.173e3 * e_charge
     rho = 0.6627
-
+    """
     # NSTX Parameters
+    device = 'NSTX'
     r = 0.37
     R = 0.94
     q = 1.7
@@ -273,7 +132,23 @@ if __name__=='__main__':
     ne = 6e19
     te = 0.45 * 1e3 * e_charge
     rho = 0.6
+    """
+    # STEP Parameters
+    device = 'SPR21'
+    r = 0.982
+    R = 3.017
+    q = 2.9919
+    shear = 0.78
+    bunit = 7.064
+    btor = 2.528
 
+    gte = 4.024
+    gne = 1.507
+
+    ne = 18.889e19
+    te = 8.599e3 * e_charge
+    rho = 0.6887
+    """
     minor_radius = r / rho
 
 
@@ -329,30 +204,28 @@ if __name__=='__main__':
     
         omegas[i] = np.real(omega / sound_speed * minor_radius)
         gammas[i] = np.imag(omega / sound_speed * minor_radius)
-          
-        
-        
-        
+
         # Test result
         minimiser = minimise_func(omega, ky, bunit, R, r, gte, gne, te, ne, k_parallel, nu, bessel, q, kperp)
         integration_diff[i] = abs(minimiser)
 
     # Save eigenvalues to file
     output = np.vstack((kyrhos, gammas,  omegas))
-    np.savetxt('mtm-integrator_eigval.dat', np.transpose(output), header='MTM-integrator ky')
+    np.savetxt(f'mtm-integrator-{device}_eigval.dat', np.transpose(output), header='MTM-integrator ky')
 
     fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True, figsize=(8,7))
 
     
     #omegas = np.interp(kyrhos,ky)
-    
-    
-    ax1.plot(kyrhos, gammas)
+
+    ax1.plot(kyrhos, gammas, label='In house code')
+
     ax1.set_ylabel(r'$c_s/a$')
     ax1.set_xlabel(r'$k_y\rho_s$')
     ax1.set_title(r'$\gamma$')
+    ax1.legend()
 
-    ax2.plot(kyrhos, omegas)
+    ax2.plot(kyrhos, omegas, label='In house code')
     ax2.set_ylabel(r'$c_s/a$')
     ax2.set_xlabel(r'$k_y\rho_s$')
     ax2.set_title(r'$\omega$')
